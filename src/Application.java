16469ee5f53fac4608c9859a2263d9801e70d409
Taimur <tsohrab8@gmail.com>
2017-06-19 09:43:43 -0400

    first commit

---
+++ src/Application.java
@@ -0,0 +1,26 @@
+
+public class Application {
+	
+	public static void main(String[]args){
+		
+		
+		System.out.println("+--------------------------------------+\n" +
+						   "|      Java Bank ATM Receipt           |\n" +
+						   "|      Wednesday, December 2, 2015     |\n" +
+						   "|      ATM Location # 123              |\n" +
+						   "|                                      |\n" +
+						   "|                                      |\n" +
+						   "|      Account Number:      1234567    |\n" +
+						   "|      Customer:     John Q. Public    |\n" +
+						   "|      Transaction Type:    Deposit    |\n" +
+						   "|      Transaction Amount:  $500.00    |\n" +
+						   "|      Account Balance:   $1,500.00    |\n" +
+						   "|                                      |\n" +
+						   "|      Thank you for banking with us   |\n" +
+						   "|            Have a coffee day         |\n" +
+						   "|                                      |\n" +
+						   "+--------------------------------------+");
+		
+	}
+
+}
